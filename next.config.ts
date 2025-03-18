388564d0ee0b581cccdfc880d51675ab6dc95a72
Valentine Omonya <valomosh254@gmail.com>
2025-03-18 03:16:50 +0300

    Updated Next.js config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "wordpress.iqonic.design",
       },
+      {
+        protocol: "https",
+        hostname: "sjc.microlink.io",
+      },
     ],
   },
 };
