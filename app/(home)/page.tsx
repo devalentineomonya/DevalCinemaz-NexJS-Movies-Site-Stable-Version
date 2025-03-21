fc4560f8bf254ec104bb2e2c0c96581ecb9cce2f
Valentine Omonya <valomosh254@gmail.com>
2025-03-21 20:20:00 +0300

    Updated home page content and layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,9 @@
-import Featured from "../features/home/widgets/Featured";
-import Hero from "../features/home/widgets/Hero";
-import TopSelection from "../features/home/widgets/TopSelection";
-import TopSeries from "../features/home/widgets/TopSeries";
-import TopTen from "../features/home/widgets/TopTen";
-import Upcoming from "../features/home/widgets/Upcoming";
+import Featured from "@/screens/home/widgets/Featured";
+import Hero from "@/screens/home/widgets/Hero";
+import TopSelection from "@/screens/home/widgets/TopSelection";
+import TopSeries from "@/screens/home/widgets/TopSeries";
+import TopTen from "@/screens/home/widgets/TopTen";
+import Upcoming from "@/screens/home/widgets/Upcoming";
 
 export default function Home() {
   return (
