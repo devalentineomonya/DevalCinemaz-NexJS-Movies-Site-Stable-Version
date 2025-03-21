1aa422062eee15363b5ccb24171f5eb01954f166
Valentine Omonya <valomosh254@gmail.com>
2025-03-21 20:20:08 +0300

    Updated Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
         hostname: "sjc.microlink.io",
       },
     ],
